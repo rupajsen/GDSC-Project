db67fbb8fede6c05bfa9e5ee767377d31be1881f
Rupaj Sen <rupajps4@gmail.com>
2022-09-09 20:23:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	componentDidMount() {
+		fetch(
+"https://jsonplaceholder.typicode.com/posts")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Create an application which fetches and displays a text from the given URL </h1> {
+				items.map((item) => (
+				<ol key = { item.id } >
+					Title: { item.title },<div></div>
+					Body: { item.body }
+					</ol>
+				))
+			}
+		</div>
+	);
+}
 }
 
 export default App;
